3a8be8cc2d7ad0978c0f5cd2089a4e8d6f259e00
Joshua Parr <jdp37803@email.vccs.edu>
2024-07-28 21:56:28 -0400

    test and major edits

--- disc_rvg_test.py
+++ disc_rvg_test.py
@@ -4,16 +4,21 @@ Created on Sun Jul 28 15:50:06 2024
 
 @author: kvmal
 """
+import random
 
-def disc_rvg (w):
-    x = rng() # placeholder name for the random number gen that khalid will write
+
+def disc_rvg(w):
+    x = random.uniform(0,1) # placeholder name for the random number gen that khalid will write
     if x <= 0.2:
         w = w + 1.2 * 60 # if assigned to agent A
-
+        a = 1
     elif x <= 0.5:
         w = w + 1.6 * 60 # if assigned to agent B
+        a = 2
     elif x <= 0.6:
         w = w + 1.35 * 60 # if assigned to agent C
+        a = 3
     else:
         w = w + 1.9 * 60 # if assigned to agent D
-    return w
+        a = 4
+    return w, x, a
--- wait_time_calc.py
+++ wait_time_calc.py
@@ -1,32 +1,61 @@
 from numpy import *
 from cmath import *
-from discrete_random_var_gen import *
+from disc_rvg_test import *
 from pandas import *
 """ n => number of dials
-    x => wait time until reaching switchboard
+    x1 => wait time until reaching switchboard
     w => total wait time in sec
-    u => random variable between 0 and 0.2296
+    u => random variable between 0 and 1 that determines
+        the time it takes to reach the switchboard
+    a => variable between 0 and 4 that designates which
+        agent was reached (0 = no agent reached, 1 = agent A, 2 = agent B, 3 = agent C, 4 = agent D)
 """
-i = 0
-for i in range(0, 499):
-    n = 0
-    w = 0
-    while n < 0:
-        x = random.uniform(0, 0.2296)
-        n += 1
-        w += 3
-        #Dial
-
-        if x < 90:
-            #to switchboard
-            w += 5
-            w += 240*u^(2/3)
-
-
+w_list = {}
+n = 0
+w = 0
+u = 0
+for i in range(0,5):
+    #beginning of caller attempts
+    end = False
+    #dial
+    n += 1
+    w += 3
+    u = random.uniform(0,1)
+    while end == False:
+        if u <= 0.7704:
+            #reaches switchboard
+            w += 5 + 240 * u**(2/3)
+            #reaches agent
+            w, u, a = disc_rvg(w)
+            if a == 0:
+                agent = 'No agent reached'
+            elif a == 1:
+                agent = 'Agent A'
+            elif a == 2:
+                agent = 'Agent B'
+            elif a == 3:
+                agent = 'Agent C'
+            else:
+                agent = 'Agent D'
+            ticket = True
+            end = True
+            break
         else:
-            #hang-up
+            #caller hangs up
             w += 90 + 2
-            #redial?
-            if n = 3:
-                print("The caller has called 3 times without getting a ticket.")
-                break
+            agent = 'No agent reached'
+            ticket = False
+            if n < 3:
+                continue
+            elif n >= 3:
+                ticket = False
+                end = True
+
+    w = round(w, 4)
+    u = round(u, 4)
+    w_list[f'{i+1}'] = u, w, n, agent, ticket
+    i += 1
+
+df = DataFrame.from_dict(w_list, orient='index', columns=['u', 'w', 'n', 'agent', 'Ticket'])
+
+print(df)
