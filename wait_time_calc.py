7fcb88095903fdf55575144376b7ad805b0da20a
Joshua Parr <jdp37803@email.vccs.edu>
2024-07-28 20:24:16 -0400

    Update wait_time_calc.py

--- wait_time_calc.py
+++ wait_time_calc.py
@@ -7,25 +7,26 @@ from pandas import *
     w => total wait time in sec
     u => random variable between 0 and 0.2296
 """
-n = 0
-w = 0
-while n < 0:
-    x = random.uniform(0, 0.2296)
-    n += 1
-    w += 3
-    #Dial
+i = 0
+for i in range(0, 499):
+    n = 0
+    w = 0
+    while n < 0:
+        x = random.uniform(0, 0.2296)
+        n += 1
+        w += 3
+        #Dial
 
-    if x < 90:
-        #to switchboard
-        w += 5
-        w += 240*u^(2/3)
+        if x < 90:
+            #to switchboard
+            w += 5
+            w += 240*u^(2/3)
 
 
-    else:
-        #hang-up
-        w += 90 + 2
-        #redial?
-        if n = 3:
-            print("The caller has called 3 times without getting a ticket.")
-            break
-
+        else:
+            #hang-up
+            w += 90 + 2
+            #redial?
+            if n = 3:
+                print("The caller has called 3 times without getting a ticket.")
+                break
