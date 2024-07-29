befb3e5860a3bb3a7252373b010c1f257fb9bfc0
Joshua Parr <jdp37803@email.vccs.edu>
2024-07-28 22:45:39 -0400

    Update wait_time_calc.py

--- wait_time_calc.py
+++ wait_time_calc.py
@@ -3,29 +3,34 @@ from cmath import *
 from disc_rvg_test import *
 from pandas import *
 """ n => number of dials
-    x1 => wait time until reaching switchboard
+    x => wait time until reaching switchboard, used in disc_rvg
     w => total wait time in sec
     u => random variable between 0 and 1 that determines
-        the time it takes to reach the switchboard
+        the time it takes to reach the switchboard; the number of u values is equal to n
     a => variable between 0 and 4 that designates which
         agent was reached (0 = no agent reached, 1 = agent A, 2 = agent B, 3 = agent C, 4 = agent D)
 """
 w_list = {}
-n = 0
-w = 0
-u = 0
-for i in range(0,5):
-    #beginning of caller attempts
+
+for i in range(0, 10):
+    n = 1
+    w = 0
+    u = 0
     end = False
-    #dial
-    n += 1
-    w += 3
-    u = random.uniform(0,1)
-    while end == False:
+    ticket = False
+    agent = 'No agent reached'
+    u_list = []
+
+    #beginning of caller attempt
+    while n <= 3 or not end:
+        # dial
+        w += 3
+        u = random.uniform(0, 1)
+        u_list.append(round(u, 4))
         if u <= 0.7704:
-            #reaches switchboard
-            w += 5 + 240 * u**(2/3)
-            #reaches agent
+            # reaches switchboard
+            w += 5 + 240 * u ** (2 / 3)
+            # reaches agent
             w, u, a = disc_rvg(w)
             if a == 0:
                 agent = 'No agent reached'
@@ -41,21 +46,22 @@ for i in range(0,5):
             end = True
             break
         else:
-            #caller hangs up
-            w += 90 + 2
+            # caller hangs up
+            w += 2
             agent = 'No agent reached'
+            n += 1
             ticket = False
-            if n < 3:
+            if n >= 3:
+                break
+            else:
                 continue
-            elif n >= 3:
-                ticket = False
-                end = True
 
     w = round(w, 4)
     u = round(u, 4)
-    w_list[f'{i+1}'] = u, w, n, agent, ticket
-    i += 1
+    w_list[f'{i+1}'] = u_list, w, n, agent, ticket
+
 
 df = DataFrame.from_dict(w_list, orient='index', columns=['u', 'w', 'n', 'agent', 'Ticket'])
 
 print(df)
+#print(w[51, 1], w[52, 1], w[53, 1],)
